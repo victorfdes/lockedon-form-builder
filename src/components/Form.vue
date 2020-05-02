4b758e7dc7678e7bb0ff8efd6fa49941d739058d
Victor Fernandes <victorfdes@users.noreply.github.com>
2020-05-02 19:05:59 +0530

    add basic form

---
+++ src/components/Form.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <v-text-field
+            label="Office ID"
+            v-model="officeID"
+            :rules="[requiredRule]"
+            filled />
+      </v-col>
+      <v-col>
+        <v-textarea
+          filled
+          :value="textAreaValue"
+          label="Embed Code" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Form',
+
+    data: () => ({
+      officeID: '',
+
+      // Rules
+      requiredRule: value => !!value || 'Required'
+    }),
+
+    computed: {
+      textAreaValue () {
+        return 'OK'
+      }
+    },
+  }
+</script>
